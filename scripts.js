5b1b7fa34457b7ffde3e2a53c90e2e796e297213
Matheus13245 <matheus.costa0020016102@edu.pe.senac.br>
2025-06-19 10:08:35 -0300

    Subindo primeira versão do projeto

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function mostrarAviso() {
+    alert("Aviso");
+}
+
+function mostrarLivros(){
+    const livros = [
+        "O Hobbit - J. R. R. Tolkien",
+        "O Nome do Vento - Patrick Rothfuss",
+        "O príncipe cruel - Holly Black",
+    ];
+}
